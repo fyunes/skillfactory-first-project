2a7266c61a3225a391fa5a11641fd0e9b5445d31
Williams Hernandez <hernandw@gmail.com>
2022-08-09 10:36:03 -0400

    :lipstick: add efect slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,25 @@ const userData = (data) => {
   user.forEach(({ id, name, email, address, phone, website }) => {
     body += `
             <div>    
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                <link rel="icon" type="image/png" sizes="192x192" href=".../icons8-user-192.png">
-                <h5 class="card-title">Nombre: <span>${name}</span> </h5>
-                <p class="card-text">id: <span>${id}</span></p>
-                <p class="card-text">Email: <span>${email}</span></p>
-                <p class="card-text">Ciudad: <span>${address.city}</span></p>
-                <p class="card-text">Teléfono: <span>${phone}</span></p>
-                <p class="card-text">Web: <span>${website}</span></p>
-                </div>
-                </div>                      
+                <div class="card" style="width: 30rem;">
+                    <div class="card-body">
+                        <div class="col--left">
+                        <img src="../assets/icons/user.png">
+                        </div>
+                        <div class="col--right">
+                            <p class="card__title">${name}</p>
+                            <p class="card__text">ID: <span>${id}</span></p>
+                            <p class="card__text">Email: <span>${email}</span></p>
+                            <p class="card__text">Ciudad: <span>${address.city}</span></p>
+                            <p class="card__text">Teléfono: <span>${phone}</span></p>
+                            <p class="card__text">Web: <span>${website}</span></p>
+                        </div>
+                    </div>
+                    <div class="card-footer"><h3>Ficha Técnica</h3></div>
+                </div>           
             </div>`;
   });
   document.getElementById("api").innerHTML = body;
 };
+
+ 
