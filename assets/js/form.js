d6121d3655ea7cf3bcd059fd07ef99f4d9147fe0
Melina Lencina <melina00lencina@gmail.com>
2022-08-11 19:24:29 -0300

    :zap: General update

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,3 @@
-
 const sendForm = () =>{
   let nameValue = document.getElementById("name").value;
   let phoneValue = document.getElementById("phone").value;
@@ -19,26 +18,22 @@ const sendForm = () =>{
 
   let body = ``;
   body += `
-    <img class="img-title" src="../assets/icons/message.png" alt="Mensaje">
-    <div class="background-mssg">
-      <h3 class="text-dimension"> From: Dimensión C-137</h3>
-      <div id="text-form">
-        <p class = "label-message">Message sent by:</p>
-        <p class = "text-message">${nameValue}</p>
+    <img class="img-title" src="../assets/icons/message.png" alt="Message">
+      <div class="background-mssg">
+        <h3 class="text-dimension"> From: Dimension C-137</h3>
+        <div id="text-form">
+          <p class = "label-message">Message sent by:</p>
+            <p class = "text-message">${nameValue}</p>
+          <p class = "label-message">Phone:</p>
+            <p class = "text-message">${phoneValue}</p>
+          <p class = "label-message">Email:</p>
+            <p class = "text-message">${emailValue}</p>
+          <p class = "label-message">Message:</p>
+            <p class = "text-message">${messageValue}</p>
+          <p class = "label-message">Contact through:
+            <p class = "text-message">${selectedContact.value}</p>
+        </div>
+      </div>`;
 
-        <p class = "label-message">Phone:</p>
-        <p class = "text-message">${phoneValue}</p>
-
-        <p class = "label-message">Email:</p>
-        <p class = "text-message">${emailValue}</p>
-
-        <p class = "label-message">Message:</p>
-        <p class = "text-message">${messageValue}</p>
-
-        <p class = "label-message">Contact through:
-        <p class = "text-message">${selectedContact.value}</p>
-    </div>
-  `;
   document.getElementById("received-message").innerHTML = body;
 };
-
